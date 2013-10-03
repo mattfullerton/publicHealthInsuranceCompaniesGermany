d7ef8bf83c622bd72bd35bcff3342fb17b8cf9bd
mattfullerton <matt.fullerton@gmail.com>
2013-10-03 14:06:51 +0200

    Deletions from script and extensions to parse HTML pages from the addresses we get from the PDF

--- scripts/simplePDFtableScraper.py
+++ scripts/simplePDFtableScraper.py
@@ -1,10 +1,12 @@
-
 # 1. Add some necessary libraries
 import scraperwiki
 import urllib2, lxml.etree
+import lxml.html
+import time
+from urlparse import urlparse
 
 # 2. The URL/web address where we can find the PDF we want to scrape
-url = 'http://cdn.varner.eu/cdn-1ce36b6442a6146/Global/Varner/CSR/Downloads_CSR/Fabrikklister_VarnerGruppen_2013.pdf'
+url = 'http://88.215.113.168/krankenkassenliste.pdf'
 
 # 3. Grab the file and convert it to an XML document we can work with
 pdfdata = urllib2.urlopen(url).read()
@@ -12,118 +14,104 @@ xmldata = scraperwiki.pdftoxml(pdfdata)
 root = lxml.etree.fromstring(xmldata)
 
 # 4. Have a peek at the XML (click the "more" link in the Console to preview it).
-print lxml.etree.tostring(root, pretty_print=True)
+#print lxml.etree.tostring(root, pretty_print=True)
 
 # 5. How many pages in the PDF document?
 pages = list(root)
-print "There are",len(pages),"pages"
+#print "There are",len(pages),"pages"
 
-'''
-# 6. Iterate through the elements in each page, and preview them
+# 6. We can use the positioning attibutes in the XML data to help us regenerate the rows and columns
+text = ''
+lastTag = 2
+recordNo = 0
+data = [] #array of records (rows)
+temp = [] #each row, added once complete
+
 for page in pages:
     for el in page:
         if el.tag == "text":
-            print el.text, el.attrib
-
-# REPLACE STEP 6 WITH THE FOLLOWING
-# 7. We can use the positioning attibutes in the XML data to help us regenerate the rows and columns
-for page in pages:
-    for el in page:
-        if el.tag == "text":
-            if int(el.attrib['left']) < 100: print 'Country:', el.text,
-            elif int(el.attrib['left']) < 250: print 'Factory name:', el.text,
-            elif int(el.attrib['left']) < 500: print 'Address:', el.text,
-            elif int(el.attrib['left']) < 1000: print 'City:', el.text,
+            if int(el.attrib['left']) < 61:
+                if (lastTag <> 0):
+                    #Add the complete last column entry
+                    temp.append(text)
+                    #Add the record
+                    data.append(temp)
+                    #clear
+                    text = ''
+                    temp = []
+                    #Add to record
+                    lastTag = 0
+                else: text += ' '
+                text += el.text
+                
+            elif int(el.attrib['left']) < 328:
+                if (lastTag <> 1):
+                    temp.append(text) #add the complete last column
+                    text = ''
+                    lastTag = 1
+                text += el.text
             else:
-                print 'Region:', el.text
+                if (lastTag <> 2):
+                    lastTag = 2;
+                    #Add the complete last column entry
+                    temp.append(text)
+                    text = '' #clear
+                else: text += ' '
+                text += el.text
+                #data[recordNo][1] = text #(re)write entry
 
-# REPLACE STEP 7 WITH THE FOLLOWING
-# 8. Rather than just printing out the data, we can generate and display a data structure representing each row.
-#    We can also skip the first page, the title page that doesn't contain any of the tabulated information we're after.
-for page in pages[1:]:
-    for el in page:
-        if el.tag == "text":
-            if int(el.attrib['left']) < 100: data = { 'Country': el.text }
-            elif int(el.attrib['left']) < 250: data['Factory name'] = el.text
-            elif int(el.attrib['left']) < 500: data['Address'] = el.text
-            elif int(el.attrib['left']) < 1000: data['City'] = el.text
-            else:
-                data['Region'] = el.text
-                print data
+for entry in data:
+    if ((len(entry) == 3) and ('Homepage' not in entry[1]) and ('essanelle' not in entry[1])):
+        url = "http://" + entry[1] + '/'
+        print 'Trying ' + url
+        try:
+            html = scraperwiki.scrape(url)
+            root = lxml.html.fromstring(html)
+            for el in root.cssselect("a"):           
+                if "mpressum" in unicode(el.text):
+                    o = urlparse(url) #In case there is a path at the end
+                    finallink = ''
+                    if ('http' in str(el.attrib['href'])): #absolute link
+                        finallink = str(el.attrib['href'])
+                    elif (str(el.attrib['href'])[0] == '/'): #link relative to root
+                        finallink = o.scheme + '://' + o.netloc + str(el.attrib['href'])
+                    else: #relative link
+                        finallink = str(url) + str(el.attrib['href'])
+                    entry.append(finallink)
+                    print 'Got ' + finallink + ', trying to get email'
+                    html = scraperwiki.scrape(finallink)
+                    root = lxml.html.fromstring(html)
+                    for el in root.cssselect("a"):
+                        if 'href' in el.attrib:
+                            '''
+                            print 'Parsing'
+                            try:
+                                o = urlparse(el.attrib['href'])
+                            except (ValueError, urllib2.HTTPError) as e:
+                                print "Invalid link... ignoring\n"
+                            if (o.scheme == 'mailto'):
+                                entry.append(str(o.netloc))
+                                print 'Got ' + str(o.netloc)
+                                break #done, don't want any others
+                            '''
+                        if (('@' in unicode(el.text)) or ('(at)' in unicode(el.text)) or (' at ' in unicode(el.text))):
+                                entry.append(el.text)
+                                print 'Got ' + str(el.text)
+                                break #done, don't want any others
+                    break #done, don't want any others
+            #print lxml.html.tostring(root, pretty_print=True)
+        except (ValueError, urllib2.HTTPError) as e:
+            print "Invalid link\n"
+        #except lxml.etree.XMLSyntaxError:
+            #print "Webpage unparseable\n"
 
-# REPLACE STEP 8 WITH THE FOLLOWING
-# 9. This really crude hack ignores data values that correspond to column headers.
-#    A more elecgant solution would use ignore elements in the first table row on each page.
-skiplist=['COUNTRY','FACTORY NAME','ADDRESS','CITY','REGION']
-for page in pages[1:]:
-    for el in page:
-        if el.tag == "text" and el.text not in skiplist:
-            if int(el.attrib['left']) < 100: data = { 'Country': el.text }
-            elif int(el.attrib['left']) < 250: data['Factory name'] = el.text
-            elif int(el.attrib['left']) < 500: data['Address'] = el.text
-            elif int(el.attrib['left']) < 1000: data['City'] = el.text
-            else:
-                data['Region'] = el.text
-                print data
+output = ''
 
-# REPLACE STEP 9 WITH THE FOLLOWING
-# 10. A crude way of adding data o the database - write each row as we scrape it.
-skiplist=['COUNTRY','FACTORY NAME','ADDRESS','CITY','REGION']
-for page in pages[1:]:
-    for el in page:
-        if el.tag == "text" and el.text not in skiplist:
-            if int(el.attrib['left']) < 100: data = { 'Country': el.text }
-            elif int(el.attrib['left']) < 250: data['Factory name'] = el.text
-            elif int(el.attrib['left']) < 500: data['Address'] = el.text
-            elif int(el.attrib['left']) < 1000: data['City'] = el.text
-            else:
-                data['Region'] = el.text
-                print data
-                scraperwiki.sqlite.save(unique_keys=[], table_name='fabvarn', data=data)
+for record in data:
+    output += '\n'
+    for col in record:
+        output += (col + ';')
 
-# REPLACE STEP 10 WITH THE FOLLOWING
-# 11. A more efficient way of writing to the database might be to write all the records scraped from a page one page at a time.
-skiplist=['COUNTRY','FACTORY NAME','ADDRESS','CITY','REGION']
-bigdata=[]
-for page in pages[1:]:
-    for el in page:
-        if el.tag == "text" and el.text not in skiplist:
-            if int(el.attrib['left']) < 100: data = { 'Country': el.text }
-            elif int(el.attrib['left']) < 250: data['Factory name'] = el.text
-            elif int(el.attrib['left']) < 500: data['Address'] = el.text
-            elif int(el.attrib['left']) < 1000: data['City'] = el.text
-            else:
-                data['Region'] = el.text
-                print data
-                bigdata.append( data.copy() )
-    scraperwiki.sqlite.save(unique_keys=[], table_name='fabvarn', data=bigdata)
-    bigdata=[]
+print output
 
-'''
-# REPLACE STEP 11 WITH THE FOLLOWING
-# 12. If necessary, and becuase we are unsing incremental rather than repeat keys,
-#     we may need to clear the database table before we right to it.
-#     A utulity function can help us do that.
 
-def dropper(table):
-    if table!='':
-        try: scraperwiki.sqlite.execute('drop table "'+table+'"')
-        except: pass
-
-dropper('fabvarn')
-
-skiplist=['COUNTRY','FACTORY NAME','ADDRESS','CITY','REGION']
-bigdata=[]
-for page in pages[1:]:
-    for el in page:
-        if el.tag == "text" and el.text not in skiplist:
-            if int(el.attrib['left']) < 100: data = { 'Country': el.text }
-            elif int(el.attrib['left']) < 250: data['Factory name'] = el.text
-            elif int(el.attrib['left']) < 500: data['Address'] = el.text
-            elif int(el.attrib['left']) < 1000: data['City'] = el.text
-            else:
-                data['Region'] = el.text
-                print data
-                bigdata.append( data.copy() )
-    scraperwiki.sqlite.save(unique_keys=[], table_name='fabvarn', data=bigdata)
-    bigdata=[]'''
